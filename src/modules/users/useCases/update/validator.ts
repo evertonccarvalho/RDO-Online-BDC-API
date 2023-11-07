6f87ac5ba58ff295a88c791cbab6d43f1ba16015
Everton Carvalho <evertoncarvalho@Everton>
2023-11-07 14:34:19 -0300

    feat: update validaton

--- src/modules/users/useCases/update/validator.ts
+++ src/modules/users/useCases/update/validator.ts
@@ -6,6 +6,9 @@ const updateValidator = {
   [Segments.BODY]: Joi.object().keys({
     usuario: Joi.string().required().min(3).max(50).messages(customMessages("name")), // Use a função customMessages
     email: Joi.string().required().min(3).max(50).messages(customMessages("email")), // Use a função customMessages
+    role: Joi.string().required().min(3).max(50).messages(customMessages("função")), // Use a função customMessages
+    ativo: Joi.boolean().messages(customMessages("ativo")), // Use a função customMessages
+    idObra: Joi.number().required().min(1).max(50).messages(customMessages("idObra")), // Use a função customMessages
   }),
 };
 
