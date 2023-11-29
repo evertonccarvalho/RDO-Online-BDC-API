5b0987bc4eb04446682427f2a97d087aa797798c
Éverton Carvalho <evertonsnake@gmail.com>
2023-11-29 09:06:51 -0300

    vercelignora e app.ts

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,22 +1,24 @@
 import cors from 'cors';
-import express, { Application } from 'express';
+import express from 'express';
 import 'reflect-metadata';
 import { errorMiddleware } from '../middleware/errors.middlewares';
 import './container';
 import { router } from './routes';
-class App {
-	public server: Application;
 
-	constructor() {
+export class App {
+	public server!: express.Application;
+
+	public constructor() {
 		this.server = express(); // Crie a instância do Express primeiro
-		this.setup();
+		this.initialize();
 	}
 
-	setup() {
+	protected initialize() {
+		this.server = express();
 		this.server.use(cors());
 		this.server.use(express.json());
 		this.server.use(errorMiddleware); // Adicione o middleware de Erros
-		this.server.use(`/`, router);
+		this.server.use(router);
 	}
 }
 
