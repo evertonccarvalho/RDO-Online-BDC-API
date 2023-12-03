bd186c32759d752de0727d592fdc5194e4d21efe
Éverton Carvalho <evertonsnake@gmail.com>
2023-12-03 11:22:50 -0300

    usario com obras nao pode ser apagado

--- src/modules/users/useCases/delete/DeleteUserController.ts
+++ src/modules/users/useCases/delete/DeleteUserController.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from 'express';
-import { container } from 'tsyringe';
-import { DeleteUserUseCase } from './DeleteUserUseCase';
 import { UserRepository } from '../../repositories/UserRepository';
+import { DeleteUserUseCase } from './DeleteUserUseCase';
 
 class DeleteUserController {
 	async handle(req: Request, res: Response): Promise<Response> {
@@ -13,7 +12,9 @@ class DeleteUserController {
 			return res.status(200).json({ message: 'Usuário deletado com sucesso' });
 		} catch (error) {
 			if (error instanceof Error) {
-				return res.status(404).json({ message: 'Usuário não encontrado' });
+				return res
+					.status(404)
+					.json({ message: 'Usuário tem obras e não pode ser apagado' });
 			}
 			return res.status(500).json({ message: 'Erro interno do servidor' });
 		}
